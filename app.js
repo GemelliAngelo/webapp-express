466ab5758bd0c9aba67ab019f46b3a8eb9f90595
GemelliAngelo <gemeangelo27@gmail.com>
2025-01-12 09:45:06 +0100

    add json parser + middlewares folder

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 // # INIT EXPRESS
 const express = require("express");
 const app = express();
+
+// # MIDDLEWARES IMPORTS
+const errorsHandler = require("./middlewares/errorHandler");
+const notFound = require("./middlewares/notFound");
+
+// # JSON PARSER FOR BODY REQUEST
+app.use(express.json());
