f9945a8855377e5aab2775b1e2e5d15eabbdb8d9
GemelliAngelo <gemeangelo27@gmail.com>
2025-01-12 11:14:04 +0100

    fix connection

--- controllers/moviescontroller.js
+++ controllers/moviescontroller.js
@@ -2,12 +2,21 @@ const connection = require("../db/movies");
 
 // # INDEX
 function index(req, res) {
-  connection.query("SELECT * FROM `movies`", (err, results) => {
-    console.log(results);
-  });
+  const sql = "SELECT * FROM movies";
 
-  res.json({
-    status: "ok",
+  connection.query(sql, (err, results) => {
+    // managing error
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: "Database query failed" });
+    }
+
+    if (!results.length) {
+      return res.status(404).json({ error: "Not Found" });
+    }
+
+    // output
+    res.json(results);
   });
 }
 
