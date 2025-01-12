3f29273b7c15596dba05a0d66e3ec738db950c0d
GemelliAngelo <gemeangelo27@gmail.com>
2025-01-12 11:10:24 +0100

    connected fix to db

--- controllers/moviescontroller.js
+++ controllers/moviescontroller.js
@@ -1,5 +1,11 @@
+const connection = require("../db/movies");
+
 // # INDEX
 function index(req, res) {
+  connection.query("SELECT * FROM `movies`", (err, results) => {
+    console.log(results);
+  });
+
   res.json({
     status: "ok",
   });
